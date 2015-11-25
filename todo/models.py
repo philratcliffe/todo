1a4265d7412b68288e5266be14efa9c6c7b1a07a
Philip Ratcliffe <phil@philratcliffe.co.uk>
2015-11-25 12:58:25 +0000

    Added done boolean field to todo model.

--- todo/models.py
+++ todo/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class ToDo(models.Model):
     todo_text = models.fields.CharField(max_length=200)
     created_date = models.DateTimeField('date created', auto_now_add=True)
+    done = models.BooleanField(default=False)
 
 
