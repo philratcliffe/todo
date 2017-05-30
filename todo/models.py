79f37f33af1371a26f961d40392d94f8a74f50e8
Phil Ratcliffe <phil@philratcliffe.co.uk>
2017-05-30 05:16:00 +0100

    Minor tidy ups

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class ToDo(models.Model):
     task_text = models.fields.CharField(max_length=200)
     created_date = models.DateTimeField('date created', auto_now_add=True)
@@ -7,5 +8,3 @@ class ToDo(models.Model):
 
     def __str__(self):
         return self.task_text
-
-
